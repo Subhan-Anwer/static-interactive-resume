41259303e61a77fa3c001e4f09706096f9e1ca66
Subhan-Anwer <sheikhanwer30@gmail.com>
2024-09-07 21:44:22 +0500

    completed

---
+++ assets/ts/app.ts
@@ -0,0 +1,99 @@
+// Show Menu
+const navMenu = document.getElementById("nav-menu") as HTMLDivElement;
+const navToggle = document.getElementById("nav-toggle") as HTMLButtonElement;
+const navClose = document.getElementById("nav-close") as HTMLButtonElement;
+
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");   
+
+  });
+}
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('.nav-menu') as NodeListOf<HTMLElement>;
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu') as HTMLDivElement;
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+// Add Blur Header
+const blurHeader = () => {
+  const header = document.getElementById('header') as HTMLDivElement;
+  // Add a class if the bottom offset is greater than 50 of the ....
+  window.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
+};
+
+window.addEventListener('scroll', blurHeader);
+
+
+
+// Email Submit
+const contactForm = document.getElementById('contact-form') as HTMLFormElement;
+const contactMessage = document.getElementById('contact-message') as HTMLParagraphElement;
+
+const sendEmail = (e: Event) => {
+  e.preventDefault();
+
+  // Simulate successful email sending
+  contactMessage.textContent = 'Message sent successfully ✅';
+
+  // Remove message after five seconds
+  setTimeout(() => {
+    contactMessage.textContent = '';
+  }, 5000);
+
+  // Clear input fields
+  contactForm.reset();   
+
+};
+
+contactForm.addEventListener('submit', sendEmail); 
+
+
+
+
+// Show Scroll Up
+const scrollUp = () => {
+    const scrollUpElement = document.getElementById('scroll-up') as HTMLDivElement;
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the
+    window.scrollY >= 350 ? scrollUpElement.classList.add('show-scroll') : scrollUpElement.classList.remove('show-scroll');
+  };
+  
+  window.addEventListener('scroll', scrollUp);
+
+
+
+  // Scroll Sections Active Link
+const section = document.querySelectorAll('section[id]') as NodeListOf<HTMLElement>;
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  section.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+    const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']') as HTMLAnchorElement;
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
